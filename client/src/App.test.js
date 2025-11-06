15c4e128047107d3237c7c9eaf2b2544752c76c3
Blissfulmyth <blissfulmyth@Krunals-MacBook-Pro.local>
2025-11-06 22:36:36 +0530

    Remove all files and folders

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the main heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/VR Floor Plan Creator/i);
-  expect(headingElement).toBeInTheDocument();
-});
